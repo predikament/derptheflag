2d5b1cd8cfe96620c7c6cea76443e3ee7af2e8bb
Thomas Dennis <dentho10@nith.no>
2013-05-29 14:58:36 +0200

    Silly.

--- src/no/nith/predikament/Game.java
+++ src/no/nith/predikament/Game.java
@@ -27,7 +27,7 @@ public class Game extends Canvas implements Runnable
 	
 	public static final int WIDTH	= 320;
 	public static final int HEIGHT	= 240;
-	public static final int SCALE	= 3;
+	public static final int SCALE	= 2;
 	
 	private boolean keepRunning;
 	
